593f69fe6f968cac3fd77c5cf5ca4eb48bc22940
Michael Stewart <michael.stewart.webdev@gmail.com>
2023-10-23 09:48:46 +0800

    fixed incorrectly placed function

--- mudlark/normalisation.py
+++ mudlark/normalisation.py
@@ -356,30 +356,3 @@ def _remove_commas(text):
         str: The modified string.
     """
     return text.replace(",", " ")
-
-
-def validate_quickgraph_id_columns(df: pd.DataFrame, id_columns: list[str]):
-    """Validate that the id_columns are present, and that they all exist
-    in the dataset.
-    This is only checked when using the QuickGraph output format, as it
-    requires an "external_id" that is comprised of the concatenation of these
-    id_columns.
-
-    Args:
-        df (pd.DataFrame): The DataFrame.
-        id_columns (list[str]): The list of id columns.
-
-    Raises:
-        ValueError: If id_columns is blank, or any cols are not in the dataset.
-    """
-    if id_columns is None:
-        raise ValueError(
-            "The id_columns argument must be set when using "
-            "the 'quickgraph' output format."
-        )
-    for col in id_columns:
-        if col not in df:
-            raise ValueError(
-                f"The column '{col}' that is listed in the "
-                "id_columns does not exist in the dataset."
-            )
