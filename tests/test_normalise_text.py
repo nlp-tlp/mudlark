0f0a9ef14a1d60a9fbd01fca94a5b73e02819101
LisonLau <101380095+LisonLau@users.noreply.github.com>
2024-03-06 15:41:18 +0800

    Fix pylint problems on test_normalise_text

--- tests/test_normalise_text.py
+++ tests/test_normalise_text.py
@@ -18,51 +18,43 @@ from mudlark import normalise_text
         ("air / con", "air conditioner"),
         ("air - con", "air conditioner"),
         ("two way", "two way radio"),
-        # ("hold", "hold"), # ! ol -> overload...
-        
+        ("hold", "hold"), # Test that it doesn't correct string within a word
         # [2] Test cases for converting text to lowercase
         ("UpperCase", "uppercase"),
         ("MixedCaSe", "mixedcase"),
         ("BROKEN", "broken"),
         ("Test Tube", "test tube"),
-        
         # [3] Test cases for adding spaces around hyphens
         ("test-tube", "test - tube"),
         ("word-hyphen-word", "word - hyphen - word"),
-        
         # [4] Test cases for removing commas
         ("test,tube", "test tube"),
         ("engine,pump,pipe", "engine pump pipe"),
-    
         # [5] Test cases for adding spaces around slashes
         ("X/X", "x / x"),
         ("test/tube", "test / tube"),
-        
         # [6] Test cases for anonymising sentences
         ("check ABX32ad and DDdkL204ddd", "check AssetID and AssetID"),
         ("AB123 XYZ789", "AssetID AssetID"),
         ("none here", "none here"),
         ("XY345Z", "AssetID"),
-        
         # [7] Test cases for removing extra spaces
         ("replace   pump", "replace pump"),
         ("  test     tube   and   test  ", "test tube and test"),
-        
         # [9] Test cases for aligning tense (irregular verbs)
         ("enGiNe was broken", "engine is broken"),
         ("many leak were Formed", "many leak are form"),
         ("metal pipe was broken", "metal pipe is broken"),
         ("gw had innadequate", "gland water has inadequate"),
-        
         # [9] Test cases for aligning tense (regular verbs)
         ("batterie running", "battery run"),
         ("worked played busted jumped", "work play bust jump"),
         ("spinning stunning", "spin stun"), # double n
         ("liked baked danced lurked", "like bake dance lurk"),
         ("hiking crying bouncing licking", "hike cry bounce lick"),
-        
         # [10] Test cases for pluralising
-        ("buses foxes bushes churches oranges houses niches", "bus fox bush church orange house niche"),  # es -> s
+        ("buses foxes bushes churches", "bus fox bush church"),  # es -> s
+        ("oranges houses niches", "orange house niche"),  # es -> s
         ("berries slurries pies", "berry slurry pie"),  # ies -> y
         ("potatoes", "potato"),  # oes -> o
         ("indices vertices", "index vertex"),  # ces -> ex 
@@ -78,7 +70,7 @@ from mudlark import normalise_text
         ("glasses classes dresses", "glass class dress"),  # sses -> ss
         ("pumps busted", "pump bust"), # pumps -> pump
         ("the foxes buys glasses", "the fox buy glass"),
-        ("situations pens focus tennis campuses", "situation pen focus tennis campus"),  # general case
+        ("pens focus tennis campuses", "pen focus tennis campus"),  # general case
     ],
 )
 def test_normalise_text_default(test_input, expected):
