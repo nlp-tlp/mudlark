2e6a4ffc1663aec7371378c16fd903b5dd462875
LisonLau <allisonlhm@gmail.com>
2024-03-26 13:12:45 +0800

    Update unit testing

--- tests/test_normalise_text.py
+++ tests/test_normalise_text.py
@@ -6,7 +6,46 @@ from mudlark import normalise_text
 @pytest.mark.parametrize(
     "test_input,expected",
     [
-        # [1] Test cases for fixing typos
+        # [1] Test cases for lowercasing
+        ("THERMOSTAT uNsErViceABLE", "thermostat unserviceable"),
+        ("UpperCase", "uppercase"),
+        ("MixedCaSe", "mixedcase"),
+        ("Test Tube", "test tube"),
+
+        # [2] Test cases for removing commas
+        ("test,tube", "test tube"),
+        ("engine,pump,pipe", "engine pump pipe"),
+        ("broken,,,pump", "broken pump"),
+
+        # [3] Test cases for removing undesirable characters
+        ("leaking !%^ in ()pipes",  "leak in pipe"),
+
+        # [4] Test cases for removing duplicate contiguous
+        ("###engine broken@@@",  "# engine broken @"),
+
+        # [5] Test cases for adding spaces around punctuations
+        ("repair?!fix'.monitor",  "repair fix . monitor"),
+        # Handle hyphens and slashes exceptions
+        ("test-tube", "test - tube"),
+        ("test/tube", "test / tube"),
+        ("o-ring long-ring", "o-ring long - ring"),
+        ("c/o replace/repair", "change out replace / repair"),
+        ("HELLO/hi hey/HEY hi/hey HI/hi", "hello/hi hey / hey hi/hey hi/hi"),
+        ("HELLO-hi hey-HEY hi-hey HI-hi", "hello-hi hey - hey hi-hey hi-hi"), 
+        ("word-hyphen-word", "word - hyphen - word"),   # !! PROBLEM
+        ("word/slash/word", "word / slash / word"),     # !! PROBLEM
+
+        # [6] Test cases for anonymising text
+        ("check ABX32ad and DDdkL204ddd", "check AssetID and AssetID"),
+        ("AB123 XYZ789", "AssetID AssetID"),
+        ("none here", "none here"),
+        ("XY345Z", "AssetID"),
+
+        # [7] Test cases for removing extra spaces
+        ("replace   pump", "replace pump"),
+        ("  test     tube   and   test  ", "test tube and test"),
+
+        # [8] Test cases for fixing typos
         ("a/c leakin", "air conditioner leak"),
         ("accum boken", "accumulator broken"),
         ("conmon", "condition monitoring"),
@@ -19,28 +58,7 @@ from mudlark import normalise_text
         ("air - con", "air conditioner"),
         ("two way", "two way radio"),
         ("hold", "hold"), # Test that it doesn't correct string within a word
-        # [2] Test cases for converting text to lowercase
-        ("UpperCase", "uppercase"),
-        ("MixedCaSe", "mixedcase"),
-        ("BROKEN", "broken"),
-        ("Test Tube", "test tube"),
-        # [3] Test cases for adding spaces around hyphens
-        ("test-tube", "test - tube"),
-        ("word-hyphen-word", "word - hyphen - word"),
-        # [4] Test cases for removing commas
-        ("test,tube", "test tube"),
-        ("engine,pump,pipe", "engine pump pipe"),
-        # [5] Test cases for adding spaces around slashes
-        ("X/X", "x/x"),
-        ("test/tube", "test / tube"),
-        # [6] Test cases for anonymising sentences
-        ("check ABX32ad and DDdkL204ddd", "check AssetID and AssetID"),
-        ("AB123 XYZ789", "AssetID AssetID"),
-        ("none here", "none here"),
-        ("XY345Z", "AssetID"),
-        # [7] Test cases for removing extra spaces
-        ("replace   pump", "replace pump"),
-        ("  test     tube   and   test  ", "test tube and test"),
+
         # [9] Test cases for aligning tense (irregular verbs)
         ("enGiNe was broken", "engine is broken"),
         ("many leak were Formed", "many leak are form"),
@@ -52,12 +70,13 @@ from mudlark import normalise_text
         ("spinning stunning", "spin stun"), # double n
         ("liked baked danced lurked", "like bake dance lurk"),
         ("hiking crying bouncing licking", "hike cry bounce lick"),
-        # [10] Test cases for pluralising
+
+        # [10] Test cases for singularising
         ("buses foxes bushes churches", "bus fox bush church"),  # es -> s
         ("oranges houses niches", "orange house niche"),  # es -> s
         ("berries slurries pies", "berry slurry pie"),  # ies -> y
         ("potatoes", "potato"),  # oes -> o
-        ("indices vertices", "index vertex"),  # ces -> ex 
+        ("indices vertices", "index vertex"),  # ces -> ex
         ("matrices appendices", "matrix appendix"),  # ces -> x
         ("elves dwarves leaves", "elf dwarf leaf"), # lves -> f, eves -> f
         ("knives wives", "knife wife"), # ives -> fe
